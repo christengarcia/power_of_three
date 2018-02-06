f653dba8364eb842d6159537d907b2622f0cbf91
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-06 21:31:20 +1100

    Initial commit

---
+++ power_of_three/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ *  C# Program to Calculate Power of Three
+ */
+
+using System;
+
+namespace power_of_three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new GeneratePowers().RaiseToPower
+               (5,    // 4 values per table
+                3);
+            Console.ReadLine(); // power to raise each value
+        }
+    }
+}
